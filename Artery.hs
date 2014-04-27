ffd81e13b4f544196a6fac17edb038bf292534a1
Patrick Boe <patrick@hametz>
2014-04-27 06:09:29 -0400

    established properties of BBox

--- Artery.hs
+++ Artery.hs
@@ -1,4 +1,16 @@
 module Artery
+  (
+   Point(Point),
+   BBox,
+   RTree(Leaf, Branch),
+   contains,
+   bound,
+   getBounds,
+   leftOf,
+   rightOf,
+   above,
+   below
+  )
   where
 
 data Point = Point Int Int
@@ -10,5 +22,17 @@ data BBox = BBox Point Point
 data RTree = Leaf BBox Int | Branch BBox RTree RTree
   deriving (Show)
 
-contains :: BBox -> BBox -> Bool
-contains = (==)
+contains x y = x == y
+
+(Point a b) `leftOf` (Point c d) = a < c
+
+(Point a b) `rightOf` (Point c d) = a > c
+
+(Point a b) `above` (Point c d) = b > d
+
+(Point a b) `below` (Point c d) = b < d
+
+bound (Point x1 y1) (Point x2 y2) =
+  BBox (Point (min x1 x2) (min y1 y2)) (Point (max x1 x2) (max y1 y2))
+
+getBounds (BBox a b) = (a, b)
--- test/Artery.Test.hs
+++ test/Artery.Test.hs
@@ -1,15 +1,21 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Artery.Test
   where
 
 import Artery
 import Control.Monad
 import Test.QuickCheck
+import Test.QuickCheck.All
+import Data.List
+
+runTests = $quickCheckAll
 
 instance Arbitrary Point where
   arbitrary = liftM2 Point arbitrary arbitrary
 
 instance Arbitrary BBox where
-  arbitrary = liftM2 BBox arbitrary arbitrary
+  arbitrary = liftM2 bound arbitrary arbitrary
 
 instance Arbitrary RTree where
   arbitrary = sized rtree'
@@ -18,5 +24,18 @@ instance Arbitrary RTree where
                       liftM3 Branch arbitrary subtree subtree]
             where subtree = rtree' (n `div` 2)
 
-prop_EqualBBoxes x = x `contains` x
-  where types = x::BBox
+prop_BoxesWithAnUncommonPointAreUnequal w x y z =
+  threeUnequal [w,x,y,z] ==> bound w x /= bound y z
+    where threeUnequal = (> 2) . length . nub
+
+prop_EqualBoxesContainEachOther b = b `contains` b
+
+prop_BoundsListBottomLeftPointFirst b =
+  case (getBounds b) of
+    (p1, p2) -> not ((p1 `rightOf` p2) || (p1 `above` p2))
+
+prop_ConstructedBoundsAreSameAsThoseOfTheOriginalPoints a@(Point x1 y1) b@(Point x2 y2) =
+  case (getBounds (bound a b)) of
+    (Point x3 y3, Point x4 y4) ->
+      (max x1 x2) == (max x3 x4) && (max y1 y2) == (max y3 y4) &&
+      (min x1 x2) == (min x3 x4) && (min y1 y2) == (min y3 y4)

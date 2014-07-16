b31f0312f310971fdf952d14fa3c7ff45692d10c
Patrick Boe <patrick@hametz>
2014-07-15 21:10:38 -0400

    strictly evaluating some elements of the benchmarks for better-isolated perf numbers

--- Benchmark/CriterionBench.hs
+++ Benchmark/CriterionBench.hs
@@ -31,7 +31,7 @@ trace =
   do bs <- generate sampleBoxes
      fes <- generate sampleEntries
      let
-       rt = buildRTree $ take 1000000 fes
+       rt = buildRTree $ take 10000 fes
        loop =
          do
            putStrLn "which box to get?"
@@ -41,26 +41,34 @@ trace =
            loop
      loop
 
+warm rt = length $ search rt (bound (Point 0 0) (Point 0 0))
+
 main =
   do es <- generate sampleEntries
      bs <- generate sampleBoxes
      fes <- generate sampleEntries
-     let buildSample n = last $ entries $ buildRTree $ take n es
+     let buildSample n = warm $ buildRTree $ take n es
          rt1000 = buildRTree $ take 1000 fes
          rt10000 = buildRTree $ take 10000 fes
          rt100000 = buildRTree $ take 100000 fes
-         rt1000000 = buildRTree $ take 1000000 fes
-         runSampleSearches rt = sum . (map $ length . (search rt)) $ take 100 bs
+         warm1 = warm rt1000
+         warm2 = warm rt10000
+         warm3 = warm rt100000
+         warm4 = warm rt1000000
+         runSampleSearches rt = sum . (map $ length . (search rt)) $ take 1000 bs
      defaultMain
        [
-         bgroup "buildRTree" [ bench "with 1000 entries" $ whnf buildSample 1000
-                             , bench "with 10000 entries" $ whnf buildSample 10000
-                             --, bench "with 1000000 entries" $ whnf buildSample 1000000
-                             ]
-
-       , bgroup "perform 100 random searches" [ bench "in a 1000 node tree" $ whnf runSampleSearches rt1000
-                                              , bench "in a 10000 node tree" $ whnf runSampleSearches rt10000
-                                              , bench "in a 100000 node tree" $ whnf runSampleSearches rt100000
-                                              , bench "in a 1000000 node tree" $ whnf runSampleSearches rt1000000
-                                              ]
+         bgroup "buildRTree"
+           [ bench "with 1000 entries" $ nf buildSample 1000
+           , bench "with 10000 entries" $ nf buildSample 10000
+           , bench "with 100000 entries" $ nf buildSample 100000
+           , bench "with 1000000 entries" $ nf buildSample 1000000
+           ]
+       , warm1 `seq` warm2 `seq` warm3 `seq` warm4 `seq`
+           bgroup "perform 1000 random searches"
+             [ bench "in a 1000 node tree" $ nf runSampleSearches rt1000
+             , bench "in a 10000 node tree" $ nf runSampleSearches rt10000
+             , bench "in a 100000 node tree" $ nf runSampleSearches rt100000
+             , bench "in a 1000000 node tree" $ nf runSampleSearches rt1000000
+             ]
        ]
